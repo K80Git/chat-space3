136e3be48738b3735c7e98357428010b6cfa1079
K80Git <67084301+K80Git@users.noreply.github.com>
2020-07-08 18:49:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:K80Git/chat-space3.git"
+set :repo_url, "git@github.com:K80Git/chat-space3.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
